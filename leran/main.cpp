21f739e3f7e67310f032665ab7dc20deaf9e918e
FunnyBear27 <47087854+FunnyBear27@users.noreply.github.com>
2022-03-24 21:52:00 +0300

    prototypes updated

--- leran/main.cpp
+++ leran/main.cpp
@@ -8,12 +8,18 @@
 
 
 // Прототипы
-void readfile(std::string file_name, int input_shape, int output_shape, std::vector<long double>);
+void readfile(std::string file_name, int input_shape, int output_shape, std::vector<std::vector<long double>> &inRef, std::vector<std::vector<long double>> &outRef);
+
+void minmax(std::vector<std::vector<long double>> &inp, long double &maximum, long double &minimum);
 void minmax(std::vector<long double> &in);
+long double maxElem(std::vector<std::vector<long double>> &inp);
 long double maxElem(std::vector<long double> &inp);
+long double minElem(std::vector<std::vector<long double>> &inp);
+long double minElem(std::vector<long double> &inp);
+
+long double errorCalc(std::vector<long double> res, std::vector<long double> true_val);
 template <typename T>
 void print(T a, std::string end = "\n");
-long double errorCalc(std::vector<long double> res, std::vector<long double> true_val);
 
 // Классы
 class Neuron{
